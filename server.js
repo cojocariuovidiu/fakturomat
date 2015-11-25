94e364f032fa8ec08bcb31849fc91ef260af2255
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2015-11-25 12:17:02 +0100

    Set up server.js to listen on port provided by config file

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+
+var app = require('./config/express');
+
+app.listen(process.env.NODE_ENV);
+console.log("Server is listening on " + process.env.NODE_ENV);
