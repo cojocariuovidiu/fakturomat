efc957921eab6cf8bad067925e9d14445557181a
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2015-11-26 07:59:28 +0100

    Front page basic setup, js libs, bootstrap

--- config/express.js
+++ config/express.js
@@ -4,5 +4,13 @@ express = require('express');
 module.exports = function(){
    var app = express();
 
+   app.set('views', './app/views');
+   app.set('x-powered-by', false);
+   app.set('view engine', 'ejs');
+
+   require('../app/routes/index.server.routes')(app);
+
+   app.use(express.static('./public'));
+   
    return app;
 }
