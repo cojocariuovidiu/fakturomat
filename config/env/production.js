c379a19168c296c780875d3c6130eeb0b682e98b
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-05-11 15:33:17 +0200

    added development.js config file'

---
+++ config/env/production.js
@@ -0,0 +1,9 @@
+var port = 1234,
+  domain = 'http://localhost';
+
+module.exports = {
+   db: 'mongodb://localhost/fakturomat',
+   sessionSecret: 'Very secret cookie secret thingy (thats how pros call that thingy!)',
+   serverPort: port,
+   domain: domain
+};
