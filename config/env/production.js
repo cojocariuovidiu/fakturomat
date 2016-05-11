e1db4809efd6133d34688257d2752bb3b607c2af
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-05-11 16:51:06 +0200

    Added mongodb config

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@ var port = 1234,
   domain = 'http://localhost';
 
 module.exports = {
-   db: 'mongodb://localhost/fakturomat',
+   db: 'mongodb://fakturomat:N6xyzR#IMLc2Z86FEC4O@ds047305.mlab.com:47305/heroku_hctb0jhj',
    sessionSecret: 'Very secret cookie secret thingy (thats how pros call that thingy!)',
    serverPort: port,
    domain: domain
